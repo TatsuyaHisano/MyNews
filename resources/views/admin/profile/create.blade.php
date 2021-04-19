7da94a27f7ef59115ea44d623d30ed11827eac14
tatsuya.hisano <ta.0317.rn@gmail.com>
2021-04-19 12:38:43 +0000

    13 ニュース投稿画面を作成しよう 課題提出

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -11,8 +11,9 @@
         <h1>プロフィール画面</h1>
         
         {{-- layouts/profile.blade.phpを読み込む --}}
+        {{-- 13ニュース投稿画面を作成しよう課題追記 --}}
         @extends('layouts.profile')
-        
+        @section('title', 'プロフィール編集')
         
         {{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
         @section('content')
@@ -20,6 +21,42 @@
                 <div class="row">
                     <div class="col-md-8 mx-auto">
                         <h2>プロフィール編集</h2>
+                        <from action="{{ action('Admin\ProfileController@create') }}" method="post"enctype="multipart/from-data">
+                            
+                            @if (count($errors) >0)
+                                <ul>
+                                    @foreach($errors->all() as $e)
+                                        <li>{{ $e }}</li>
+                                    @endforeach
+                                </ul>
+                            @endif
+                            <div class="from-group row">
+                                <lavel class="col-md-2">氏名(name)</lavel>
+                                <div class="col-md-10">
+                                    <input type="text" class="from-control"name="title" value="{{ old('title') }}">
+                                </div>
+                            </div>
+                            <div class="from-group row">
+                                <lavel class="col-md-2">性別(gender)</lavel>
+                                <div class="col-md-10">
+                                    <input type="text" class="from-control"name="gender" value="{{ old('gender') }}">
+                                </div>
+                            </div>
+                            <div class="from-group row">
+                                <lavel class="col-md-2">趣味(hobby)</lavel>
+                                <div class="col-md-10">
+                                    <input type="text" class="from-control"name="hobby" value="{{ old('hobby') }}">
+                                </div>
+                            </div>
+                            <div class="from-group row">
+                                <lavel class="col-md-2">自己紹介(introduction)</lavel>
+                                <div class="col-md-10">
+                                    <textarea class="form-control" name="introduction" rows="20">{{old('introduction') }}</textarea>
+                                </div>
+                            </div>
+                            {{ csrf_field() }}
+                            <input type="submit" class="btn btn-primary" value="更新">
+                        </from>
                     </div>
                 </div>
             </div>
